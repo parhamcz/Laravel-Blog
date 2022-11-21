812f4ef99bc5f487cae5f51c7f06f656157708f7
parham <parham.safaiyan@gmail.com>
2022-11-22 00:08:02 +0330

    first commit

---
+++ Quera Project/Modules/Admin/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Admin\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->check()) {
+            if(auth()->user()->isAdmin()){
+                return $next($request);
+            }
+        }
+        return redirect('/');
+    }
+}
---
+++ Quera Project/Modules/Admin/Routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    
+    Route::get('/', 'AdminController@index');
+    Route::resource('/users','AdminUserController');
+    Route::resource('/posts','AdminPostController');
+});
