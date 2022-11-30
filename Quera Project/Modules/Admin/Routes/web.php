3c5c0790b0110f1d51345d1cac2be3a1da5c9b2b
Parham Safaiyan <parham.safaiyann@gmail.com>
2022-11-30 23:59:42 +0330

    admin routes added

--- Quera Project/Modules/Admin/Routes/web.php
+++ Quera Project/Modules/Admin/Routes/web.php
@@ -1,20 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 use Illuminate\Support\Facades\Route;
+use Modules\Admin\Http\Controllers\AdminTagController;
+use Modules\Admin\Http\Controllers\AdminPostController;
+use Modules\Admin\Http\Controllers\AdminCategoryController;
 
-Route::prefix('admin')->name('admin.')->group(function() {
-    
-    Route::get('/panel', 'AdminController@index');
-    Route::resource('/users','AdminUserController');
-    Route::resource('/posts','AdminPostController');
+Route::prefix('/admin')->group(function () {
+
+    Route::resource('/posts', AdminPostController::class);
+    Route::get('/posts-data', [AdminPostController::class, 'data'])->name('posts.data');
+    Route::post('/activate/posts/{post}', [AdminPostController::class, 'activate'])->name('posts.activate');
+
+    Route::resource('/categories', AdminCategoryController::class);
+    Route::get('/categories-data', [AdminCategoryController::class, 'data'])->name('categories.data');
+
+    Route::resource('/tags', AdminTagController::class);
+    Route::get('/tags-data', [AdminTagController::class, 'data'])->name('tags.data');
 });
