e60f6f283ec685fc7190f1817c398b4d925efd3b
parham <parham.safaiyann@gmail.com>
2022-12-03 23:22:18 +0330

    DataTable Trait,Tag and Comment Model,Mig and Controller added!

---
+++ Quera Project/app/Traits/DataTable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+
+trait DataTable
+{
+    protected static function generateDataTable(Request $request)
+    {
+        // TODO: Implement
+    }
+}
