a2138961285cc368a2a0656b8bd8f0e735c4fe1f
parham <parham.safaiyann@gmail.com>
2022-12-03 23:11:59 +0330

    admin module controllers pt.1 : adminPostController configured

--- Quera Project/Modules/Admin/Http/Controllers/AdminPostController.php
+++ Quera Project/Modules/Admin/Http/Controllers/AdminPostController.php
@@ -27,19 +27,37 @@ class AdminPostController extends Controller
 
     public function activate(Post $post)
     {
-        // TODO
+        if($post->isApproved()) {
+            $post->update([
+                'approved' => false,
+            ]);
+        } else {
+            $post->update([
+                'approved' => true,
+            ]);
+        }
         return redirect(route('posts.index'));
     }
 
     public function create()
     {
-        // TODO
+        $categories = Category::latest()->whereNull('parent_id')->get();
+        $tags = Tag::latest()->get();
         return view('admin::posts.create', compact('categories', 'tags'));
     }
 
     public function store(Request $request)
     {
-        // TODO
+        $inputs = $request->except(['categories', 'image']);
+        $imagesUrl = $this->uploadImages($request->file('image'), '/images/posts');
+        if(!request()->filled('slug')) {
+            $inputs['slug'] = $this->createSlug($request->title, '-');
+        }
+        $post = auth()->user()->posts()->create(
+            array_merge($inputs, ['images' => $imagesUrl])
+        );
+        $post->categories()->sync($request->input('categories'));
+        $post->tags()->sync($request->input('tags'));
         return redirect()->route('posts.index');
     }
 
@@ -50,13 +68,26 @@ class AdminPostController extends Controller
 
     public function edit(Post $post)
     {
-        // TODO
+        $categories = Category::latest()->whereNull('parent_id')->get();
+        $tags = Tag::latest()->get();
         return view('admin::posts.edit', compact('post', 'categories', 'tags'));
     }
 
     public function update(Request $request, Post $post)
     {
-        // TODO
+        $inputs = $request->except(['categories']);
+        if(!request()->filled('slug')) {
+            $inputs['slug'] = $this->createSlug($request->title, '-');
+        }
+        if($request->hasFile('image')) {
+            Storage::disk('public')->delete('/' . $post->images);
+            $inputs['images'] = $this->uploadImages($request->file('image'), '/images/posts');
+        } else {
+            $inputs['images'] = $post->images;
+        }
+        $post->update($inputs);
+        $post->categories()->sync($request->input('categories'));
+        $post->tags()->sync($request->input('tags'));
         return redirect()->route('posts.index');
     }
 
